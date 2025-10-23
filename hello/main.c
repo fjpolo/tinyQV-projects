8caff6dac9271ec777eb0c1e5b9e6679d1cce75f
fjpolo <fjpolo@gmail.com>
2025-10-23 16:23:19 +0200

    Add while(1) loop

--- hello/main.c
+++ hello/main.c
@@ -6,17 +6,19 @@
 int a = 3;
 
 int main() {
-  #ifdef TEST_PRINTF
-  printf("Hello, world!\n");
-  printf("Hello %d\n", a);
-  a = mul32x16(12, a);
-  printf("Hello %d\n", a);
-  printf("Cycles %d, time %d, instret %d\n", read_cycle(), read_time(), read_instret());
+  while(1){
+#ifdef TEST_PRINTF
+    printf("Hello, world!\n");
+    printf("Hello %d\n", a);
+    a = mul32x16(12, a);
+    printf("Hello %d\n", a);
+    printf("Cycles %d, time %d, instret %d\n", read_cycle(), read_time(), read_instret());
 #else
-  for(char c='A';c<='Z';++c){
-    uart_putc(c);
-    for(int i=0;i<10000;++i);
-  }
+    for(char c='A';c<='Z';++c){
+      uart_putc(c);
+      for(int i=0;i<10000;++i);
+    }
 #endif
+  }
   return 0;
 }
