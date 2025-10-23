63e7f879c362ea38790902cc84f9a5176b4afcf1
fjpolo <fjpolo@gmail.com>
2025-10-23 16:22:19 +0200

    Use putc instead of printf

--- hello/main.c
+++ hello/main.c
@@ -6,10 +6,17 @@
 int a = 3;
 
 int main() {
+  #ifdef TEST_PRINTF
   printf("Hello, world!\n");
   printf("Hello %d\n", a);
   a = mul32x16(12, a);
   printf("Hello %d\n", a);
   printf("Cycles %d, time %d, instret %d\n", read_cycle(), read_time(), read_instret());
+#else
+  for(char c='A';c<='Z';++c){
+    uart_putc(c);
+    for(int i=0;i<10000;++i);
+  }
+#endif
   return 0;
 }
